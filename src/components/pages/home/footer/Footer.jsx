f81ff3c644fcda7ef1c3413bd41d360fcb396d52
chisator <gianfrancosacchi@hotmail.com.ar>
2023-06-01 23:46:41 -0300

    arreglando estilos del footer y header

--- src/components/pages/home/footer/Footer.jsx
+++ src/components/pages/home/footer/Footer.jsx
@@ -5,33 +5,39 @@ export const Footer = () => {
     return (
         <footer className='container-fluid'>
             <div className='row'>
-                <div>
+                <div className='col-md-5 d-flex align-items-center justify-content-center' >
                     <h1 className='footer-logo' >ASCENDERE</h1>
                 </div>
-                <div>
-                    <h5>La Empresa</h5>
-                    <ul>
-                        <li><a href="">Quienes somos</a></li>
-                        <li><a href="">Misión</a></li>
-                        <li><a href="">Visión</a></li>
-                        <li><a href="">Valores</a></li>
-                    </ul>
+                <div className='col-md-5'>
+                    <div className='row'>
+                        <div className='col-md-5'>
+                            <h5 className='h5'>La Empresa</h5>
+                            <ul>
+                                <li><a href="">Quienes somos</a></li>
+                                <li><a href="">Misión</a></li>
+                                <li><a href="">Visión</a></li>
+                                <li><a href="">Valores</a></li>
+                            </ul>
+                        </div>
+
+                        <div className='col-md-7'>
+                            <h5 className='h5'>Servicios</h5>
+                            <ul>
+                                <li><a href="">Clima Laboral</a></li>
+                                <li><a href="">Autoliderazgo</a></li>
+                                <li><a href="">Consultoría de RR.HH.</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <h5>Servicios</h5>
-                    <ul>
-                        <li><a href="">Clima Laboral</a></li>
-                        <li><a href="">Autoliderazgo</a></li>
-                        <li><a href="">Consultoría de RR.HH.</a></li>
-                    </ul>
+                <div className='col-md-2 d-flex flex-column justify-content-between'>
+                    <li><a href="" className='contacto'>Contacto</a></li>
+                    <a href="">
+                        <LinkedInIcon style={{ fontSize: "40px" }} color="secondary"></LinkedInIcon>
+                    </a>
                 </div>
-                <div>
-                    <a href="">
-                        <h5>Contacto</h5>
-                    </a>
-                    <a href="">
-                        <LinkedInIcon color="secondary"></LinkedInIcon>
-                    </a>
+                <div className="col-sm-12 text-center">
+                    <hr />
                 </div>
             </div>
         </footer>
--- src/components/pages/home/header/Header.jsx
+++ src/components/pages/home/header/Header.jsx
@@ -4,35 +4,35 @@ import { Link } from "react-router-dom";
 
 
 export const Header = () => {
-    const [visible, setVisible]= useState('visible')
+  const [visible, setVisible] = useState('visible')
 
   let prevScrollPos = window.pageYOffset;
-  window.onscroll = function() {
+  window.onscroll = function () {
     const currentScrollPos = window.pageYOffset;
     if (prevScrollPos > currentScrollPos) {
       setVisible('visible')
-    } else{
-        setVisible(' ')   
-      } 
-      prevScrollPos = currentScrollPos;
+    } else {
+      setVisible(' ')
     }
+    prevScrollPos = currentScrollPos;
+  }
   return (
     <header className={visible}>
-        <div className="container-xl">
-          <div className='row'>
-            <div className='col-xs-6 col-sm-9 '>
-              <a href="#" className='d-flex align-items-center'>
-                <img className='logo-header' src={logo} alt="" />
-                
-              </a>
-            </div>
-            <div className='col-xs-6 col-sm-3  d-flex justify-content-around align-items-center'>
-              <Link to="/contact"><button><span className="text">Contactanos</span></button></Link>
-            </div>
+      <div className="container">
+        <div className='row'>
+          <div className='col-sm-9 col-6 '>
+            <a href="#" className='d-flex align-items-center'>
+              <img className='logo-header' src={logo} alt="" />
 
+            </a>
+          </div>
+          <div className='col-sm-3 col-6  d-flex justify-content-around align-items-center'>
+            <Link to="/contact"><button><span className="text">Contactanos</span></button></Link>
           </div>
 
         </div>
-      </header>
+
+      </div>
+    </header>
   )
 }
