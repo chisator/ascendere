ae438422fa5799f5f46e83dd911c5c9fb4c7f493
chisator <gianfrancosacchi@hotmail.com.ar>
2023-06-05 17:18:53 -0300

    Agregando el menu para disp  <768px

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,46 +1,72 @@
-
-import LinkedInIcon from '@mui/icons-material/LinkedIn'
-import './Footer.css'
-import { Link } from 'react-router-dom'
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 export const Footer = () => {
-    return (
-        <footer className='container-fluid'>
-            <div className='row'>
-                <div className='col-md-5 d-flex align-items-center justify-content-center' >
-                    <h1 className='footer-logo'>ASCENDERE</h1>
-                </div>
-                <div className='col-md-5'>
-                    <div className='row'>
-                        <div className='col-md-5'>
-                            <h5 className='h5'>La Empresa</h5>
-                            <ul>
-                                <li><a href="">Quienes somos</a></li>
-                                <li><a href="">Misión</a></li>
-                                <li><a href="">Visión</a></li>
-                                <li><a href="">Valores</a></li>
-                            </ul>
-                        </div>
-
-                        <div className='col-md-7'>
-                            <h5 className='h5'>Servicios</h5>
-                            <ul>
-                                <li><a href="">Clima Laboral</a></li>
-                                <li><a href="">Autoliderazgo</a></li>
-                                <li><a href="">Consultoría de RR.HH.</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className='col-md-2 d-flex flex-column justify-content-between'>
-                    <li><Link to={"/contact"} className='contacto'>Contacto</Link></li>
-                    <a href="">
-                        <LinkedInIcon style={{ fontSize: "40px" }} color="secondary"></LinkedInIcon>
-                    </a>
-                </div>
-                <div className="col-sm-12 text-center">
-                    <hr />
-                </div>
+  return (
+    <footer className="container-fluid">
+      <div className="row">
+        <div className="col-md-5 d-flex align-items-center justify-content-center">
+          <h1 className="footer-logo">ASCENDERE</h1>
+        </div>
+        <div className="col-md-5">
+          <div className="row">
+            <div className="col-md-5">
+              <h5 className="h5">La Empresa</h5>
+              <ul>
+                <li>
+                  <a href="">Quienes somos</a>
+                </li>
+                <li>
+                  <a href="">Misión</a>
+                </li>
+                <li>
+                  <a href="">Visión</a>
+                </li>
+                <li>
+                  <a href="">Valores</a>
+                </li>
+              </ul>
             </div>
-        </footer>
-    )
-}
+
+            <div className="col-md-7">
+              <h5 className="h5">Servicios</h5>
+              <ul>
+                <li>
+                  <a href="">Clima Laboral</a>
+                </li>
+                <li>
+                  <a href="">Autoliderazgo</a>
+                </li>
+                <li>
+                  <a href="">Consultoría de RR.HH.</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-2 d-flex flex-column justify-content-between">
+          <li>
+            <Link to={"/contact"} className="contacto">
+              Contacto
+            </Link>
+          </li>
+          <a href="">
+            <LinkedInIcon
+              style={{ fontSize: "40px" }}
+              color="secondary"
+            ></LinkedInIcon>
+          </a>
+        </div>
+        <div className="col-sm-12 text-center">
+          <hr />
+        </div>
+        <div className="col-sm-12 d-flex justify-content-center align-items-center">
+          <p>
+            ASCENDERE - People Developement - Consultoria en Recursos Humanos -
+            Buenos Aires, Argentina.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,58 +1,117 @@
-import { useState } from "react";
-import logo from '../../../assets/img/logo-sin-fondo.png'
+import { useRef, useState } from "react";
+import logo from "../../../assets/img/logo-sin-fondo.png";
 import { Link } from "react-router-dom";
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export const Header = () => {
-  const [visible, setVisible] = useState('visible')
+  const line1BarsRef = useRef(null);
+  const line2BarsRef = useRef(null);
+  const line3BarsRef = useRef(null);
 
+  const animateBars = () => {
+    line1BarsRef.current.classList.toggle("activeline1__bars-menu");
+    line2BarsRef.current.classList.toggle("activeline2__bars-menu");
+    line3BarsRef.current.classList.toggle("activeline3__bars-menu");
+    setIsShow(!isShow);
+  };
+  const [isShow, setIsShow] = useState(false);
+  const [visible, setVisible] = useState("visible");
+  const [isHovered, setIsHovered] = useState(false);
+
+  let handleHover = () => {
+    setIsHovered(true);
+  };
+  const handleHoverDisable = () => {
+    setIsHovered(false);
+  };
   let prevScrollPos = window.pageYOffset;
   window.onscroll = function () {
     const currentScrollPos = window.pageYOffset;
     if (prevScrollPos > currentScrollPos) {
-      setVisible('visible')
+      setVisible("visible");
     } else {
-      setVisible(' ')
+      setVisible(" ");
+      setIsShow(false)
+      line1BarsRef.current.classList.remove("activeline1__bars-menu");
+      line2BarsRef.current.classList.remove("activeline2__bars-menu");
+      line3BarsRef.current.classList.remove("activeline3__bars-menu");
     }
     prevScrollPos = currentScrollPos;
-  }
+  };
   return (
     <header className={visible}>
-      <div className="container">
-        <div className='row'>
-          <div className='col-sm-3 col-4 '>
-            <Link to={"/"} className='d-flex align-items-center'>
-              <img className='logo-header' src={logo} alt="" />
-
+      <nav className="navbar navbar-expand-md bg-body-tertiary">
+        <div className="container-fluid">
+          <div className="col-sm-3 col-4">
+            <Link
+              to={"/"}
+              style={{ width: "100px" }}
+              className="m-auto d-flex align-items-center"
+            >
+              <img className="logo-header" src={logo} alt="" />
             </Link>
           </div>
-          <div className="col-sm-6 col-3 d-flex align-items-center">
-            <ul className="d-flex align-items-center justify-content-evenly gap-5">
-              <li><Link to={"/la-empresa"}>La Empresa</Link></li>
-              <li><div className="dropdown">
-                <a className=""  data-bs-toggle="dropdown" >
-                  Servicios <KeyboardArrowUpIcon/>
-                </a>
+          <div onClick={() => animateBars()} className="bars__menu">
+            <span className="line1__bars-menu" ref={line1BarsRef}></span>
+            <span className="line2__bars-menu" ref={line2BarsRef}></span>
+            <span className="line3__bars-menu" ref={line3BarsRef}></span>
+          </div>
+          <div className={`navbar-ul col-8 ${isShow ? "show" : ""}`}>
+            <ul
+              className={`${
+                isShow ? "show" : ""
+              }navbar-nav`}
+            >
+              <div className="nav-empresa-servicio">
+                <li>
+                  <Link to={"/la-empresa"}>La Empresa</Link>
+                </li>
+                <li>
+                  <div className="dropdown">
+                    <a
+                      onMouseEnter={handleHover}
+                      onMouseLeave={handleHoverDisable}
+                      className={isHovered ? "show" : ""}
+                      data-bs-toggle="dropdown"
+                    >
+                      Servicios <KeyboardArrowUpIcon />
+                    </a>
 
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Clima Laboral</a></li>
-                  <li><a className="dropdown-item" href="#">Autoliderazgo</a></li>
-                  <li><a className="dropdown-item" href="#">Consultoria de RRHH</a></li>
-                </ul>
+                    <ul
+                      className={
+                        isHovered ? "show dropdown-menu" : "dropdown-menu"
+                      }
+                      onMouseEnter={handleHover}
+                      onMouseLeave={handleHoverDisable}
+                    >
+                      <li>
+                        <a className="" href="#">
+                          Clima Laboral
+                        </a>
+                      </li>
+                      <li>
+                        <a className="" href="#">
+                          Autoliderazgo
+                        </a>
+                      </li>
+                      <li>
+                        <a className="" href="#">
+                          Consultoria de RRHH
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
               </div>
-              </li>
+              <div className="nav-contact col-sm-3 col-5">
+                <li>
+                  <Link to="/contact">Contactanos</Link>
+                </li>
+              </div>
             </ul>
           </div>
-          <div className='col-sm-3 col-5  d-flex justify-content-around align-items-center'>
-            <li>
-              <Link to="/contact">Contactanos</Link>
-            </li>
-          </div>
-
         </div>
-
-      </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
