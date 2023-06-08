ca28f54f6ae972dbb61b589d35148eccf47c7bea
chisator <gianfrancosacchi@hotmail.com.ar>
2023-06-08 17:19:04 -0300

    Terminando la pagina servicios

--- src/components/pages/servicios/Servicios.jsx
+++ src/components/pages/servicios/Servicios.jsx
@@ -1,11 +1,32 @@
 import { Footer } from "../../common/footer/Footer";
 import img1 from "../../../assets/img/img1.webp";
-import img2 from "../../../assets/img/img2.webp"
-import img3 from "../../../assets/img/img3.webp"
+import img2 from "../../../assets/img/img2.webp";
+import img3 from "../../../assets/img/img3.webp";
 import { Header } from "../../common/header/Header";
 import "./Servicios.css";
 
 export const Servicios = () => {
+  const listItem = document.querySelectorAll("#subnav-serv nav ul li a");
+  console.log(listItem);
+  const backdrop = document.querySelector("#menu-backdrop");
+  listItem.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      const { left, top, width, height } = item.getBoundingClientRect();
+
+      backdrop.style.setProperty("--left", `${left}px`);
+      backdrop.style.setProperty("--top", `${top-top+5}px`);
+      backdrop.style.setProperty("--width", `${width}px`);
+      backdrop.style.setProperty("--height", `${height}px`);
+
+      backdrop.style.opacity = "1";
+      backdrop.style.visibility = "visible";
+    });
+
+    item.addEventListener("mouseleave", () => {
+      backdrop.style.opacity = "0";
+      backdrop.style.visibility = "hidden";
+    });
+  });
   return (
     <>
       <Header />
@@ -16,41 +37,44 @@ export const Servicios = () => {
         </div>
       </div>
       <div className="page-servicios">
-        <nav>
-          <ul className="d-flex align-items-center justify-content-center p-1">
-            <li>
-              <a
-                className="active subnav"
-                data-bs-target="#carouselExampleDark"
-                data-bs-slide-to="0"
-                aria-current="true"
-                aria-label="Slide 1"
-              >
-                Clima Laboral
-              </a>
-            </li>
-            <li>
-              <a
-                className="subnav"
-                data-bs-target="#carouselExampleDark"
-                data-bs-slide-to="1"
-                aria-label="Slide 2"
-              >
-                Autoliderazgo
-              </a>
-            </li>
-            <li>
-              <a
-                className="subnav"
-                data-bs-target="#carouselExampleDark"
-                data-bs-slide-to="2"
-                aria-label="Slide 3"
-              >
-                Consultoria
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <header id="subnav-serv">
+          <nav>
+            <ul className="d-flex align-items-center justify-content-center p-1">
+              <li className="">
+                <a
+                  className="active subnav"
+                  data-bs-target="#carouselExampleDark"
+                  data-bs-slide-to="0"
+                  aria-current="true"
+                  aria-label="Slide 1"
+                >
+                  Clima Laboral
+                </a>
+              </li>
+              <li className="">
+                <a
+                  className="subnav"
+                  data-bs-target="#carouselExampleDark"
+                  data-bs-slide-to="1"
+                  aria-label="Slide 2"
+                >
+                  Autoliderazgo
+                </a>
+              </li>
+              <li className="">
+                <a
+                  className="subnav"
+                  data-bs-target="#carouselExampleDark"
+                  data-bs-slide-to="2"
+                  aria-label="Slide 3"
+                >
+                  Consultoria
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div id="menu-backdrop"></div>
+        </header>
         <div id="carouselExampleDark" className="carousel carousel-dark slide">
           <div className="carousel-indicators">
             <button
@@ -79,33 +103,95 @@ export const Servicios = () => {
               className="primer-serv carousel-item active"
               data-bs-interval="10000"
             >
-              <div className=" carousel-caption d-none d-md-block">
-                <img src={img1} alt="" />
-                <h5 className="">¿Como mejorar el clima laboral?</h5>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Odio, voluptatibus id, necessitatibus at earum aperiam cumque,
-                  laudantium voluptatum fugit asperiores pariatur ipsa itaque?
-                  Fugiat possimus exercitationem iusto optio dolore doloribus.
-                </p>
+              <div className="h-100 d-flex align-items-center container">
+                <div className="  container-serv">
+                  <div className="col-12 col-md-8 d-flex align-items-center justify-content-center">
+                    <h3 className="">¿Como mejorar el clima laboral?</h3>
+                  </div>
+                  <picture className="col-12 col-md-5">
+                    <img src={img1} alt="" />
+                  </picture>
+                  <div className="col-12 col-md-3">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Odio, voluptatibus id, necessitatibus at earum aperiam
+                      cumque, laudantium voluptatum fugit asperiores pariatur
+                      ipsa itaque? Fugiat possimus exercitationem iusto optio
+                      dolore doloribus.
+                    </p>
+                  </div>
+                  <div className="col-12 col-md-8">
+                    <h6>Subtitulo</h6>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Exercitationem inventore fuga soluta alias ad ea.
+                      Aspernatur repellendus ipsa aliquid adipisci blanditiis
+                      mollitia voluptatem unde quae magni fugit hic, libero
+                      similique.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="carousel-item" data-bs-interval="2000">
-              <div className="carousel-caption d-none d-md-block">
-                <img src={img2} alt="" />
-                <h5>Second slide label</h5>
-                <p>
-                  Some representative placeholder content for the second slide.
-                </p>
+              <div className="h-100 d-flex align-items-center container">
+                <div className=" container-serv ">
+                  <div className="col-12 col-md-8 d-flex align-items-center justify-content-center">
+                    <h3>Autoliderazgo</h3>
+                  </div>
+                  <picture className="col-12 col-md-5">
+                    <img src={img2} alt="" />
+                  </picture>
+                  <div className="col-12 col-md-3">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid temporibus quod aliquam consequuntur, sapiente
+                      odit vitae asperiores molestias tempora animi culpa, esse
+                      incidunt distinctio numquam veniam itaque dolorum.
+                      Nostrum, vero.
+                    </p>
+                  </div>
+                  <div className="col-12 col-md-8">
+                    <h6>Subtitulo</h6>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Exercitationem inventore fuga soluta alias ad ea.
+                      Aspernatur repellendus ipsa aliquid adipisci blanditiis
+                      mollitia voluptatem unde quae magni fugit hic, libero
+                      similique.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="carousel-item">
-              <div className="carousel-caption d-none d-md-block">
-                <img src={img3} alt="" />
-                <h5>Third slide label</h5>
-                <p>
-                  Some representative placeholder content for the third slide.
-                </p>
+              <div className="h-100 d-flex align-items-center container">
+                <div className="container-serv ">
+                  <div className="col-12 col-md-8 d-flex align-items-center justify-content-center">
+                    <h3>Consultoria de Recursos Humanos</h3>
+                  </div>
+                  <picture className="col-12 col-md-5">
+                    <img src={img3} alt="" />
+                  </picture>
+                  <div className="col-12 col-md-3">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Autem magni nihil vitae excepturi maiores explicabo,
+                      itaque officiis voluptatum esse illo temporibus in minus
+                      expedita aspernatur tempore beatae modi cumque. Sint!
+                    </p>
+                  </div>
+                  <div className="col-12 col-md-8">
+                    <h6>Subtitulo</h6>
+                    <p>
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Exercitationem inventore fuga soluta alias ad ea.
+                      Aspernatur repellendus ipsa aliquid adipisci blanditiis
+                      mollitia voluptatem unde quae magni fugit hic, libero
+                      similique.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
